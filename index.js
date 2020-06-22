04353f1cc7aef68af789f3c9a6f15d9ea1951774
azifcodes <61082527+azifcodes@users.noreply.github.com>
2020-06-22 16:49:12 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const txtInp = document.getElementById("txtInp");
+const keyInp = document.getElementById("keyInp");
+const btn = document.getElementById("btn");
+const txtOut = document.getElementById("txtOut");
+
+btn.onclick = function()
+{
+		let encTxt = CryptoJS.AES.encrypt(txtInp.value, keyInp.value).toString();
+		
+		txtOut.textContent = encTxt;
+}
